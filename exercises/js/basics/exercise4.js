f6e16cd1b88bbd27d523e212e1c1aa17f5f8c809
esoey <145457989+esoey@users.noreply.github.com>
2024-01-25 14:02:19 +0100

    exercises

--- exercises/js/basics/exercise4.js
+++ exercises/js/basics/exercise4.js
@@ -1,1 +1,21 @@
-/* implement the BankAccount class here */
+class BankAccount{
+    constructor(name, balance){
+        this.name = name;
+        this.balance = balance;
+    }
+
+deposit(amount){
+    this.balance = this.balance + amount
+}
+
+withdraw(amount){
+    if (this.balance >= amount){
+        this.balance = this.balance - amount;
+        return true
+    }
+    else {
+        console.log("Insufficient funds!")
+        return false
+    }
+}
+}
